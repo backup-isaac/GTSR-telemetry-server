1a40949f2380c765329621f3fcc4d95f1243a8e0
Jared Duncan <jduncan45@gatech.edu>
2018-08-01 23:12:06 -0400

    Added data recording startup

--- main.go
+++ main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
+	"log"
+
+	"github.gatech.edu/GTSR/telemetry-server/datatypes"
 	"github.gatech.edu/GTSR/telemetry-server/listener"
+	"github.gatech.edu/GTSR/telemetry-server/storage"
 )
 
 func main() {
-	listener.Listen()
+	go listener.Listen()
+	err := recordData()
+	log.Fatalf("Error recording data: %s", err)
+}
+
+func recordData() error {
+	points := make(chan *datatypes.Datapoint)
+	err := listener.Subscribe(points)
+	store, err := storage.NewStorage()
+	if err != nil {
+		return err
+	}
+	defer store.Close()
+	for {
+		err = store.Insert([]*datatypes.Datapoint{<-points})
+		if err != nil {
+			return err
+		}
+	}
 }
