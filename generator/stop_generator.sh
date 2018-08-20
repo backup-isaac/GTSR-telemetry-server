82c2be7043906fc818251b1159e98aee8ee97827
Jared Duncan <jduncan45@gatech.edu>
2018-08-20 17:19:55 -0400

    Deterministically name containers

--- generator/stop_generator.sh
+++ generator/stop_generator.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker exec -it $(docker ps | grep telemetry-server_server | awk '{print $NF}') /bin/sh -c 'kill $(ps -aux | grep generator | awk "{print \$2}")'
+docker exec -it server /bin/sh -c 'kill $(ps -aux | grep generator | awk "{print \$2}")'
