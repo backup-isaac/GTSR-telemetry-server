966d1a6be3bd111846ef69e3bf3b1389dd49021b
Jared Duncan <jduncan45@gatech.edu>
2018-09-06 20:52:00 -0700

    Check that username is provided in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+if [[ $# -eq 0 ]] ; then
+    echo 'Usage: ./deploy.sh <your GT username>'
+    exit 1
+fi
 rsync -r . $1@mefsvs01.me.gatech.edu:~/telemetry-server
 ssh -t $1@mefsvs01.me.gatech.edu "sudo cp -r ~/telemetry-server /opt/telemetry-server; cd /opt/telemetry-server; sudo /usr/local/bin/docker-compose up -d"
