0ceee4f3bb908269037caf6bb474d02b95a14849
Jared Duncan <jduncan45@gatech.edu>
2018-09-06 22:00:28 -0700

    Removed slack key (revoked previously committed)

--- chatbot/get.py
+++ chatbot/get.py
@@ -1,9 +1,15 @@
 import requests
 import re
+import json
+
+with open('secrets.json', 'r') as secretsfile:
+    secrets = json.load(secretsfile)
+    slack_key = secrets['slack-key']
+
 with open('test.txt', 'a') as myfile:
     for page in range(1,41):
         try:
-            r = requests.get('https://slack.com/api/search.messages?token=xoxp-6242112129-21687605060-253849597012-bc80f4ea03616a32194b30746e280ed9&query=from%3A%40jackson.merkl%20-%22shared%20a%20file%3A%22%20-%22uploaded%20a%20file%3A%22&count=40&page={}&pretty=1'.format(page))
+            r = requests.get('https://slack.com/api/search.messages?token={}&query=from%3A%40jackson.merkl%20-%22shared%20a%20file%3A%22%20-%22uploaded%20a%20file%3A%22&count=40&page={}&pretty=1'.format(slack_key, page))
             for message in r.json()['messages']['matches']:
                 if not 'http' in message['text'] and message['channel']['name'] != 'team-leadership' and message['channel']['name'] != 'electrical-leadership':
                     new_message = re.sub(r'<!channel> ', '', message['text'])
