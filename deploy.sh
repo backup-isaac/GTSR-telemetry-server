dbfadeb152a59b5511bd55e89d1cf433962f5bc1
Michael Lin <michael.lin@gatech.edu>
2018-10-26 13:16:31 -0700

    Updated deploy script to use systemd

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,8 @@ if [[ $# -eq 0 ]] ; then
     echo 'Usage: ./deploy.sh <your GT username>'
     exit 1
 fi
+echo "Now copying to ~/telemetry-server"
 rsync -r . $1@mefsvs01.me.gatech.edu:~/telemetry-server
-ssh -t $1@mefsvs01.me.gatech.edu "sudo cp -r ~/telemetry-server /opt; cd /opt/telemetry-server; sudo /usr/local/bin/docker-compose up -d --force-recreate"
+echo "Now copying to /opt/telemetry-server and restarting (This takes a while)"
+ssh -t $1@mefsvs01.me.gatech.edu "sudo systemctl stop telemetry-server; sudo cp -r ~/telemetry-server /opt; cd /opt/telemetry-server; sudo systemctl start telemetry-server"
+
