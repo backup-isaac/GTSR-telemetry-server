7d9674a09385afd856eba8e60f6d4fa535440036
Michael Lin <michael.lin@gatech.edu>
2018-11-26 01:59:35 -0500

    Added HTTPs support to our services Seperate out nginx configs for dev and production to support http locally removed aws script

--- deploy-aws.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rsync -r . ubuntu@18.221.138.32:~/telemetry-server
-ssh -t ubuntu@18.221.138.32 "sudo cp -r ~/telemetry-server /opt; cd /opt/telemetry-server; sudo docker-compose up -d --force-recreate"
--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,3 @@ echo "Now copying to ~/telemetry-server"
 rsync -r . $1@mefsvs01.me.gatech.edu:~/telemetry-server
 echo "Now copying to /opt/telemetry-server and restarting (This takes a while)"
 ssh -t $1@mefsvs01.me.gatech.edu "sudo systemctl stop telemetry-server; sudo cp -r ~/telemetry-server /opt; cd /opt/telemetry-server; sudo systemctl start telemetry-server"
-
