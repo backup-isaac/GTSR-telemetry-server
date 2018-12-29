7eef59796ea89466aa9a758665fc6d03ab8a58d8
Michael Lin <michael.lin@gatech.edu>
2018-12-29 00:22:55 -0500

    Updated to new file structure

--- rf-listener/build.bat
+++ rf-listener/build.bat
@@ -1,2 +1,3 @@
-if not exist "go\src" mkdir go\src
-docker run --rm=true -it -v %cd%:/go/src/rf-listener -v %cd%/go/src:/go/src -w /go/src/rf-listener golang:1.11.2 /bin/bash -c "go get go.bug.st/serial.v1 && GOOS=linux GOARCH=amd64 go build -o bin/listen listen.go && GOOS=darwin GOARCH=amd64 go build -o bin/listen.app listen.go && GOOS=windows GOARCH=amd64 go build -o bin/listen.exe listen.go"
+docker volume create go-cache
+
+docker run --rm=true -it -v %cd%:/go/src/rf-listener -v go-cache:/go/src -w /go/src/rf-listener golang:1.11.2 /bin/bash -c "go get go.bug.st/serial.v1 && GOOS=linux GOARCH=amd64 go build -o bin/listen listen.go && GOOS=darwin GOARCH=amd64 go build -o bin/listen.app listen.go && GOOS=windows GOARCH=amd64 go build -o bin/listen.exe listen.go"
