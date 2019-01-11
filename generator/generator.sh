ae6d773ff817adaf56b0cd2d87408bf92b3b51ec
Jared Duncan <jduncan45@gatech.edu>
2019-01-11 16:33:47 -0500

    Removed unnecessary Dockerfile

--- generator/generator.sh
+++ generator/generator.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-docker build -t generator $DIR
-docker run --rm -t -i --name generator --network="telemetry-server_default" generator go run data_generator.go $1
+docker run --rm -t -i --name generator -v $DIR:/app --network="telemetry-server_default" golang:1.11.2 go run /app/data_generator.go $1
