1b65c19da6ead140ae18edae2dd5ae7955b344c7
Jared Duncan <jduncan45@gatech.edu>
2019-10-22 19:06:52 -0400

    Added assertion that can config metric names are unique

--- server/configs/configs_test.go
+++ server/configs/configs_test.go
@@ -11,12 +11,17 @@ func TestConfigs(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Error loading configs: %v", err)
 	}
+	existingNames := make(map[string]bool)
 	// Check that there are no illegal metric names in the config
 	for _, configList := range configs {
 		for _, config := range configList {
 			if !storage.ValidMetric(config.Name) {
 				t.Errorf("Illegal metric name: %v", config.Name)
 			}
+			if existingNames[config.Name] {
+				t.Errorf("Found duplicate metric name in CAN configs")
+			}
+			existingNames[config.Name] = true
 		}
 	}
 }
