14cbc70be8a1463717dc4709016293921f4c5ea8
Isaac Weintraub <iweintraub3@gatech.edu>
2019-11-19 13:37:01 -0500

    ReconTool basic front end

--- server/main.go
+++ server/main.go
@@ -27,6 +27,7 @@ func main() {
 		api.NewFoodHandler(),
 		api.NewJacksonHandler(),
 		api.NewMapHandler(),
+		api.NewReconToolHandler(),
 	})
 	go computations.RunComputations()
 	err = recordData(store)
