63a638d74aebfd4fa06b6fd1470282d5b28ad7f4
svong7 <svong7@gatech.edu>
2020-01-16 20:52:43 -0500

    Update merge.go

--- server/api/merge.go
+++ server/api/merge.go
@@ -39,5 +39,5 @@ func (m *MergeHandler) RegisterRoutes(router *mux.Router) {
 	router.PathPrefix("/merge/static/").Handler(http.StripPrefix("/merge/static/", http.FileServer(http.Dir(path.Join(dir, "merge")))))
 
 	router.HandleFunc("/merge", m.MergeDefault).Methods("GET")
-  router.HandleFunc("/merge", m.MergePost).Methods("POST")
+  router.HandleFunc("/merge_post", m.MergePost).Methods("POST")
 }
