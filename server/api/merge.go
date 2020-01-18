70632f332f93b6bd7873ada2cf9694b73873c3bc
Nick Chaloult <npchaloult@gmail.com>
2020-01-18 15:12:48 -0500

    Created new POST /merge handler which receives form data
    When you visit /merge in a browser, you're met with a page that asks you
    to specify what time range you'd like to merge points on local storage
    from. This handler receievs the data that users enter in that form, and
    will pass it along to the place where the actual point-merging logic
    happens in the future.

--- server/api/merge.go
+++ server/api/merge.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"path"
@@ -25,6 +26,22 @@ func (m *MergeHandler) MergeDefault(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/merge/static/index.html", http.StatusFound)
 }
 
+// MergePointsHandler receives form data from the site at "/merge".
+func (m *MergeHandler) MergePointsHandler(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		// TODO: Write a better error message omg
+		http.Error(w, "Failed to parse form data: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(w, "form data: %s, %s, %s",
+		r.Form.Get("timezone-offset"),
+		r.Form.Get("start-time"),
+		r.Form.Get("end-time"),
+	)
+}
+
+// RegisterRoutes registers the routes for the merge service.
 func (m *MergeHandler) RegisterRoutes(router *mux.Router) {
 	_, filename, _, ok := runtime.Caller(0)
 	if !ok {
@@ -34,4 +51,5 @@ func (m *MergeHandler) RegisterRoutes(router *mux.Router) {
 	router.PathPrefix("/merge/static/").Handler(http.StripPrefix("/merge/static/", http.FileServer(http.Dir(path.Join(dir, "merge")))))
 
 	router.HandleFunc("/merge", m.MergeDefault).Methods("GET")
+	router.HandleFunc("/merge", m.MergePointsHandler).Methods("POST")
 }
