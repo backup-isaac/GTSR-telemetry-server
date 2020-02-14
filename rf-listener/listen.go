216a0de3746d99fd3cfce2580aef167c981d3186
abhaycashikar <abhaycashikar@gmail.com>
2020-02-13 20:03:44 -0500

    Implemented state machine for parsing bytes

--- rf-listener/listen.go
+++ rf-listener/listen.go
@@ -71,88 +71,113 @@ func main() {
 	buf := make([]byte, 144)
 	// if CRC enabled, use a different algorithm to read in bytes
 	if len(os.Args) > 3 && os.Args[3] == "CRC" {
-		bytesRead := 0
-		incompleteBuf := make([]byte, 16)
-		checkGT := true
+		var packetIndex = 0
+		var packetBuffer = make([]byte, 16)
 		for {
-			n, err := s.Read(buf)
+			numBytes, err := s.Read(buf)
 			if err != nil {
 				log.Fatalf("Serial error: %s", err)
 			}
-			// if this is the first iteration or CRC was failed in the previous iteration, look for a GT
-			if checkGT {
-				// if the first two characters between the incomplete buffer and read buffer aren't GT, reset bytesRead and look for a new GT in buf
-				if (bytesRead == 0) || (bytesRead == 1 && (incompleteBuf[0] != 'G' || buf[0] != 'T')) || incompleteBuf[0] != 'G' || incompleteBuf[1] != 'T' {
-					bytesRead = 0
-					for i := 0; i < n; i++ {
-						if buf[i] == 'G' {
-							if i+1 >= n {
-								incompleteBuf[0] = 'G'
-								bytesRead = 1
-							} else if buf[i+1] == 'T' {
-								checkGT = false
-								break
-							}
+			for i := 0; i < numBytes; i++ {
+				parseByte(buf[i], &packetIndex, packetBuffer)
+				if err != nil {
+					continue
+				}
+				if packetIndex == len(packetBuffer) {
+					packetIndex = 0
+					if verifyChecksum(packetBuffer, table) {
+						_, err := conn.Write(packetBuffer[:12])
+						if err != nil {
+							log.Fatalf("Error writing to connection: %s", err)
 						}
+					} else {
+						log.Printf("CRC failed")
 					}
-					if checkGT {
-						continue
-					}
-				} else {
-					checkGT = false
 				}
-			}
-			// check if we need to add bytes to an incomplete frame
-			start := 0
-			if bytesRead > 0 {
-				if n+bytesRead < 16 {
-					// add bytes to existing incomplete frame
-					copy(incompleteBuf[bytesRead:bytesRead+n], buf[:])
-					bytesRead += n
-					continue
-				} else {
-					// finish incomplete frame, and offset start position in buf
-					copy(incompleteBuf[bytesRead:16], buf[:16-bytesRead])
-					start += 16 - bytesRead
-					// reset bytesRead to 0
-					bytesRead = 0
-				}
-			}
-			// create a new buffer to add uncompromised frames to
-			cleanBuf := make([]byte, 144)
-			var i int
-			j := 0
-			// if incompleteBuf was finished, do CRC then add it to cleanBuf to be written if passed
-			if start > 0 {
-				if !verifyChecksum(incompleteBuf, table) {
-					log.Println("CRC failed.")
-					checkGT = true
-				} else {
-					log.Println("CRC passed!")
-					copy(cleanBuf[:12], incompleteBuf[:12])
-					j = 12
-				}
-			}
-			// for each complete frame, do CRC, then add to cleanBuf if passed
-			for i = start; i < n-15; i += 16 {
-				if !verifyChecksum(buf[i:i+16], table) {
-					log.Println("CRC failed.")
-					checkGT = true
-					continue
-				}
-				log.Println("CRC passed!")
-				copy(cleanBuf[j:j+12], buf[i:i+12])
-				j += 12
-			}
-			// add extra bytes (if any) to incompleteBuf
-			copy(incompleteBuf[:n-i+1], buf[i:n])
-			bytesRead = n - i + 1
-			// write complete frames to TCP
-			_, err = conn.Write(cleanBuf[:j])
-			if err != nil {
-				log.Fatalf("Error writing to connection: %s", err)
 			}
 		}
+		// bytesRead := 0
+		// incompleteBuf := make([]byte, 16)
+		// checkGT := true
+		// for {
+		// 	n, err := s.Read(buf)
+		// 	if err != nil {
+		// 		log.Fatalf("Serial error: %s", err)
+		// 	}
+		// 	// if this is the first iteration or CRC was failed in the previous iteration, look for a GT
+		// 	if checkGT {
+		// 		// if the first two characters between the incomplete buffer and read buffer aren't GT, reset bytesRead and look for a new GT in buf
+		// 		if (bytesRead == 0) || (bytesRead == 1 && (incompleteBuf[0] != 'G' || buf[0] != 'T')) || incompleteBuf[0] != 'G' || incompleteBuf[1] != 'T' {
+		// 			bytesRead = 0
+		// 			for i := 0; i < n; i++ {
+		// 				if buf[i] == 'G' {
+		// 					if i+1 >= n {
+		// 						incompleteBuf[0] = 'G'
+		// 						bytesRead = 1
+		// 					} else if buf[i+1] == 'T' {
+		// 						checkGT = false
+		// 						break
+		// 					}
+		// 				}
+		// 			}
+		// 			if checkGT {
+		// 				continue
+		// 			}
+		// 		} else {
+		// 			checkGT = false
+		// 		}
+		// 	}
+		// 	// check if we need to add bytes to an incomplete frame
+		// 	start := 0
+		// 	if bytesRead > 0 {
+		// 		if n+bytesRead < 16 {
+		// 			// add bytes to existing incomplete frame
+		// 			copy(incompleteBuf[bytesRead:bytesRead+n], buf[:])
+		// 			bytesRead += n
+		// 			continue
+		// 		} else {
+		// 			// finish incomplete frame, and offset start position in buf
+		// 			copy(incompleteBuf[bytesRead:16], buf[:16-bytesRead])
+		// 			start += 16 - bytesRead
+		// 			// reset bytesRead to 0
+		// 			bytesRead = 0
+		// 		}
+		// 	}
+		// 	// create a new buffer to add uncompromised frames to
+		// 	cleanBuf := make([]byte, 144)
+		// 	var i int
+		// 	j := 0
+		// 	// if incompleteBuf was finished, do CRC then add it to cleanBuf to be written if passed
+		// 	if start > 0 {
+		// 		if !verifyChecksum(incompleteBuf, table) {
+		// 			log.Println("CRC failed.")
+		// 			checkGT = true
+		// 		} else {
+		// 			log.Println("CRC passed!")
+		// 			copy(cleanBuf[:12], incompleteBuf[:12])
+		// 			j = 12
+		// 		}
+		// 	}
+		// 	// for each complete frame, do CRC, then add to cleanBuf if passed
+		// 	for i = start; i < n-15; i += 16 {
+		// 		if !verifyChecksum(buf[i:i+16], table) {
+		// 			log.Println("CRC failed.")
+		// 			checkGT = true
+		// 			continue
+		// 		}
+		// 		log.Println("CRC passed!")
+		// 		copy(cleanBuf[j:j+12], buf[i:i+12])
+		// 		j += 12
+		// 	}
+		// 	// add extra bytes (if any) to incompleteBuf
+		// 	copy(incompleteBuf[:n-i+1], buf[i:n])
+		// 	bytesRead = n - i + 1
+		// 	// write complete frames to TCP
+		// 	_, err = conn.Write(cleanBuf[:j])
+		// 	if err != nil {
+		// 		log.Fatalf("Error writing to connection: %s", err)
+		// 	}
+		// }
 	}
 	// otherwise, receive messages from serial port
 	for {
@@ -190,4 +215,26 @@ func verifyChecksum(buf []byte, table *crc32.Table) bool {
 	checksumTransmitted := binary.LittleEndian.Uint32(buf[len(buf)-4:])
 	checksumCalculated := crc32.Checksum(buf[:len(buf)-4], table)
 	return checksumTransmitted == checksumCalculated
+}
+
+func parseByte(b byte, packetIndex *int, packetBuffer []byte) error {
+	switch *packetIndex {
+	case 0:
+		if b != 'G' {
+			return fmt.Errorf("G not found")
+		}
+		packetBuffer[*packetIndex] = b
+		*packetIndex++
+	case 1:
+		if b != 'T' {
+			*packetIndex = 0
+			return fmt.Errorf("T not found")
+		}
+		packetBuffer[*packetIndex] = b
+		*packetIndex++
+	default:
+		packetBuffer[*packetIndex] = b
+		*packetIndex++
+	}
+	return nil
 }
