01319cfb9765ca7a555fe0fc4ef5de4bd0b297c9
abhaycashikar <abhaycashikar@gmail.com>
2020-02-15 16:18:17 -0500

    Added file equivalence check

--- rf-listener/listen_test.go
+++ rf-listener/listen_test.go
@@ -1,27 +1,28 @@
 package main
 
 import (
-	"fmt"
 	"io"
-	"log"
+	"io/ioutil"
 	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestReadWriteBytes(t *testing.T) {
 	testInput, err := os.Open("test_input.bin")
-	if err != nil {
-		log.Fatalf("Test input open error: %s", err)
-	}
+	assert.NoError(t, err)
 	defer testInput.Close()
 	testOutput, err := os.Create("test_output.bin")
-	if err != nil {
-		log.Fatalf("Test output open error: %s", err)
-	}
+	assert.NoError(t, err)
 	defer testOutput.Close()
 	err = readWriteBytes(testInput, testOutput)
 	if err != io.EOF {
-		log.Fatalf("Read/write failed: %s", err)
+		assert.Fail(t, err.Error())
 	}
-	fmt.Printf("Output stored in %s", testOutput.Name())
+	inBytes, err := ioutil.ReadAll(testInput)
+	assert.NoError(t, err)
+	outBytes, err := ioutil.ReadAll(testOutput)
+	assert.NoError(t, err)
+	assert.EqualValues(t, inBytes, outBytes)
 }
