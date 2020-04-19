a5e35f3076d0cb01a895de47501cee802fcc212f
Abhay Cashikar <abhaycashikar@gatech.edu>
2020-04-19 14:50:26 -0500

    Added test for checking corrupted frames

--- rf-listener/listen_test.go
+++ rf-listener/listen_test.go
@@ -7,8 +7,10 @@ import (
 	"io"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -21,17 +23,15 @@ func TestMain(m *testing.M) {
 }
 
 func setup() {
-	// Do something here.
+	// Generate all required test input files.
 	generateTestInput()
 	generateCompleteCRCTestInput()
-	fmt.Printf("\033[1;36m%s\033[0m", "> Setup completed\n")
+	generateIncompleteCRCTestInput()
+	fmt.Print("> Setup completed\n")
 }
 
 func teardown() {
-	// Do something here.
-
-	fmt.Printf("\033[1;36m%s\033[0m", "> Teardown completed")
-	fmt.Printf("\n")
+	fmt.Print("> Teardown completed\n")
 }
 
 // TestReadWriteBytes checks whether readWriteBytes writes the bytes from test_input.bin to test_output.bin correctly.
@@ -90,51 +90,32 @@ func TestReadWriteBytesCompleteCRC(t *testing.T) {
 	assert.EqualValues(t, expectedBytes, actualBytes)
 }
 
-// // TestReadWriteBytesCRC checks whether readWriteBytes reads bytes from test_input_crc.bin and outputs only frames that pass CRC to test_output_crc.bin.
-// func TestReadWriteBytesCRC(t *testing.T) {
-// 	testInput, err := os.Open("test_input_crc.bin")
-// 	assert.NoError(t, err)
-// 	defer testInput.Close()
-// 	testOutput, err := os.Create("test_output_crc.bin")
-// 	assert.NoError(t, err)
-// 	defer testOutput.Close()
-// 	err = readWriteBytesCRC(testInput, testOutput)
-// 	if err != io.EOF {
-// 		assert.Fail(t, err.Error())
-// 	}
-// 	expectedOutput, err := os.Open("expected_output_crc.bin")
-// 	inBytes, err := ioutil.ReadAll(expectedOutput)
-// 	assert.NoError(t, err)
-// 	outBytes, err := ioutil.ReadAll(testOutput)
-// 	assert.NoError(t, err)
-// 	assert.EqualValues(t, inBytes, outBytes)
-// }
-
-// TestReadWriteBytesCRCComplete checks whether readWriteBytesCRC reads bytes from test_input_crc_complete.bin and outputs only frames that pass CRC to test_output_crc_complete.bin.
-// func TestReadWriteBytesCRCComplete(t *testing.T) {
-// 	testInput, err := os.Open("test_input_crc_complete.bin")
-// 	assert.NoError(t, err)
-// 	defer testInput.Close()
-// 	testOutput, err := os.Create("test_output_crc_complete.bin")
-// 	assert.NoError(t, err)
-// 	defer testOutput.Close()
-// 	err = readWriteBytesCRC(testInput, testOutput)
-// 	if err != io.EOF {
-// 		assert.Fail(t, err.Error())
-// 	}
-// 	testOutput.Close()
-// 	expectedOutput, err := os.Open("test_input.bin")
-// 	assert.NoError(t, err)
-// 	defer expectedOutput.Close()
-// 	expectedBytes, err := ioutil.ReadAll(expectedOutput)
-// 	assert.NoError(t, err)
-// 	actualOutput, err := os.Open("test_output_crc_complete.bin")
-// 	assert.NoError(t, err)
-// 	defer actualOutput.Close()
-// 	actualBytes, err := ioutil.ReadAll(actualOutput)
-// 	assert.NoError(t, err)
-// 	assert.EqualValues(t, expectedBytes, actualBytes)
-// }
+// TODO: write test to parse incomplete frames+CRC, write only complete frames
+func TestReadWriteBytesIncompleteCRC(t *testing.T) {
+	testInput, err := os.Open("test_input_incomplete_crc.bin")
+	assert.NoError(t, err)
+	defer testInput.Close()
+	testOutput, err := os.Create("test_output.bin")
+	assert.NoError(t, err)
+	defer testOutput.Close()
+	err = readWriteBytesCRC(testInput, testOutput)
+	if err != io.EOF {
+		assert.Fail(t, err.Error())
+	}
+	expectedOutput, err := os.Open("test_expected_output_incomplete_crc.bin")
+	assert.NoError(t, err)
+	defer expectedOutput.Close()
+	// Reset reader
+	testOutput.Close()
+	testOutput, err = os.Open("test_output.bin")
+	assert.NoError(t, err)
+	defer testOutput.Close()
+	expectedBytes, err := ioutil.ReadAll(expectedOutput)
+	assert.NoError(t, err)
+	actualBytes, err := ioutil.ReadAll(testOutput)
+	assert.NoError(t, err)
+	assert.EqualValues(t, expectedBytes, actualBytes)
+}
 
 // Test input generation functions (called in setup)
 
@@ -158,7 +139,7 @@ func generateTestInput() {
 		buf = append(buf, valBuf...)
 		_, err = outFile.Write(buf)
 		if err != nil {
-			log.Fatalf("Creation of test input failed: %s", err.Error())
+			log.Fatalf("Write to test input failed: %s", err.Error())
 		}
 		floatValue -= 512
 	}
@@ -189,7 +170,67 @@ func generateCompleteCRCTestInput() {
 		buf = append(buf, checksumBuf...)
 		_, err = outFile.Write(buf)
 		if err != nil {
-			log.Fatalf("Creation of complete CRC test input failed: %s", err.Error())
+			log.Fatalf("Write to complete CRC test input failed: %s", err.Error())
+		}
+		floatValue -= 512
+	}
+}
+
+// TODO: write generator for incomplete frame+CRC test input
+// Mock corruption of data by randomly removing a byte from the frame every now and then
+func generateIncompleteCRCTestInput() {
+	table := crc32.MakeTable(crc32.IEEE)
+	outFile, err := os.Create("test_input_incomplete_crc.bin")
+	if err != nil {
+		log.Fatalf("Creation of incomplete CRC test input failed: %s", err.Error())
+	}
+	defer outFile.Close()
+	postParseFile, err := os.Create("test_expected_output_incomplete_crc.bin")
+	if err != nil {
+		log.Fatalf("Creation of incomplete CRC expected output failed: %s", err.Error())
+	}
+	defer outFile.Close()
+	GTAndCANID := make([]byte, 4)
+	binary.BigEndian.PutUint32(GTAndCANID, 0x4754FFFF)
+	var floatValue uint16 = 0xC842
+	zeroBuf := make([]byte, 6)
+	valBuf := make([]byte, 2)
+	checksumBuf := make([]byte, 4)
+	// Seed rand with current time, so that test is unique
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	// Was the previous frame corrupted?
+	wasCorrupted := false
+	for i := 0; i < 100; i++ {
+		binary.BigEndian.PutUint16(valBuf, floatValue)
+		buf := make([]byte, 0)
+		buf = append(buf, GTAndCANID...)
+		buf = append(buf, zeroBuf...)
+		buf = append(buf, valBuf...)
+		checksum := crc32.Checksum(buf, table)
+		binary.LittleEndian.PutUint32(checksumBuf, checksum)
+		if !wasCorrupted {
+			randNum := r.Intn(100)
+			if randNum > 11 {
+				// This file should only be written to if the frame is not thrown out by readWriteBytesCRC
+				_, err = postParseFile.Write(buf)
+				if err != nil {
+					log.Fatalf("Write to incomplete CRC expected output failed: %s", err.Error())
+				}
+			} else {
+				// The next frame is not thrown out if 'G' or 'T' is never seen
+				if randNum > 1 {
+					wasCorrupted = true
+				}
+				// Remove the byte in buf corresponding to randNum
+				buf = append(buf[:randNum], buf[randNum+1:]...)
+			}
+		} else {
+			wasCorrupted = false
+		}
+		buf = append(buf, checksumBuf...)
+		_, err = outFile.Write(buf)
+		if err != nil {
+			log.Fatalf("Write to incomplete CRC test input failed: %s", err.Error())
 		}
 		floatValue -= 512
 	}
