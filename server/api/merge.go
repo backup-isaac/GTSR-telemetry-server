0384fe68be10ceb14b77bf795947a53c685783da
Nick Chaloult <npchaloult@gmail.com>
2020-09-12 13:56:49 -0400

    Add polling for the status of async browser->local server point uploads

--- server/api/merge.go
+++ server/api/merge.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"path"
 	"runtime"
+	"sync/atomic"
 	"time"
 
 	"server/api/merge"
@@ -16,6 +17,19 @@ import (
 
 	"github.com/gorilla/mux"
 )
+
+// isUploading keeps track of whether we're currently sending a collection of
+// datapoints from someone's web browser to a local server instance.
+//
+// merge_info_config.json stores info about the status of a merge operation from
+// a local server to the remote server - not from a browser to a local server.
+// Keeping track of this info in merge_info_config.json also wouldn't be a good
+// idea beacuse reading to and writing from files aren't atomic operations.
+var isUploading atomic.Value
+
+func init() {
+	isUploading.Store(false)
+}
 
 // MergeHandler handles requests related to merging points from a local
 // instance of the server onto the main remote server.
@@ -34,9 +48,37 @@ func NewMergeHandler(store *storage.Storage) *MergeHandler {
 	return &MergeHandler{merger, store}
 }
 
+type uploadRequest struct {
+	startTime *time.Time
+	endTime   *time.Time
+}
+
+var uploadRequestQueue = make(chan uploadRequest)
+
+// processUploadRequests is a goroutine that listens for new merge requests on
+// the uploadRequestQueue chan. When a user fills out the form on the site at
+// "/merge", m.LocalMergeHandler() validates that form data, then creates a new
+// uploadRequest which will be picked up here.
+func (m *MergeHandler) processUploadRequests() {
+	for request := range uploadRequestQueue {
+		isUploading.Store(true)
+		m.uploadLocalPointsToRemote(request.startTime, request.endTime)
+		isUploading.Store(false)
+	}
+}
+
 // MergeDefault is the default handler for the /merge path.
 func (m *MergeHandler) MergeDefault(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "/merge/static/index.html", http.StatusFound)
+	if !isUploading.Load().(bool) {
+		http.Redirect(w, r, "/merge/static/index.html", http.StatusFound)
+	} else {
+		http.Redirect(w, r, "/merge/static/uploading.html", http.StatusFound)
+	}
+}
+
+// IsUploading handles requests at the /merge/isUploading route.
+func (m *MergeHandler) IsUploading(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(isUploading.Load().(bool))
 }
 
 // LocalMergeHandler receives form data from the site at "/merge".
@@ -46,13 +88,7 @@ func (m *MergeHandler) LocalMergeHandler(w http.ResponseWriter, r *http.Request)
 		http.Error(w, err.Error(), statusCode)
 		return
 	}
-
-	// TODO: temporary!
-	// Eventually, have the web page react to the status code that we send
-	// here. Perhaps the web page could append some paragraph tag to the DOM
-	// that either says everything went smoothly or that an error occurred.
-	fmt.Fprintln(w, "Points collected locally have been merged successfully")
-	// w.WriteHeader(http.StatusNoContent)
+	w.WriteHeader(http.StatusNoContent)
 }
 
 // localMergeHandlerHelper performs the business logic for input validation and
@@ -84,12 +120,14 @@ func (m *MergeHandler) localMergeHandlerHelper(r *http.Request) (int, error) {
 			)
 	}
 
-	// Begin the upload process to the remote server
-	err = m.merger.UploadLocalPointsToRemote(startTime, endTime)
-	if err != nil {
-		return http.StatusInternalServerError,
-			fmt.Errorf("Error uploading local datapoints to remote server: %v",
-				err.Error())
+	// Create an uploadRequest to upload our local points to the remote server.
+	select {
+	case uploadRequestQueue <- uploadRequest{startTime, endTime}:
+		// No-op. Just push the new uploadRequest onto the queue.
+	default:
+		return http.StatusLocked,
+			fmt.Errorf("Another request to upload points from a local server" +
+				" to the remote server is in progress.")
 	}
 
 	return -1, nil
@@ -117,6 +155,18 @@ func (m *MergeHandler) RemoteMergeHandler(w http.ResponseWriter, r *http.Request
 
 	// Everything went alright. Respond with 204.
 	w.WriteHeader(http.StatusNoContent)
+}
+
+// uploadLocalPointsToRemote uses information in an uploadRequest to kick off
+// the process of merging datapoints on a local server instance to the remote
+// server.
+func (m *MergeHandler) uploadLocalPointsToRemote(startTime, endTime *time.Time) {
+	err := m.merger.UploadLocalPointsToRemote(startTime, endTime)
+	if err != nil {
+		errMsg := fmt.Errorf("Error uploading local datapoints to remote"+
+			" server: %v", err.Error())
+		log.Println(errMsg)
+	}
 }
 
 // Turns date/timezone strings into RFX3339Nano time.Time types.
@@ -149,6 +199,8 @@ func formatRFC3339(date string, timezone string) (*time.Time, error) {
 
 // RegisterRoutes registers the routes for the merge service.
 func (m *MergeHandler) RegisterRoutes(router *mux.Router) {
+	go m.processUploadRequests()
+
 	_, filename, _, ok := runtime.Caller(0)
 	if !ok {
 		log.Fatal("Could not find runtime caller")
@@ -157,6 +209,7 @@ func (m *MergeHandler) RegisterRoutes(router *mux.Router) {
 	router.PathPrefix("/merge/static/").Handler(http.StripPrefix("/merge/static/", http.FileServer(http.Dir(path.Join(dir, "merge")))))
 
 	router.HandleFunc("/merge", m.MergeDefault).Methods("GET")
+	router.HandleFunc("/merge/isUploading", m.IsUploading).Methods("GET")
 	router.HandleFunc("/merge", m.LocalMergeHandler).Methods("POST")
 	router.HandleFunc("/remotemerge", m.RemoteMergeHandler).Methods("POST")
 }
